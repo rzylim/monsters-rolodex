8e5b1704a14dbd29763bb568d1347e3b02fe7ee5
Zhen Yi Lim <zhen.yi@outlook.com>
2020-01-27 22:38:25 +0800

    Updates

---
+++ precache-manifest.91f1ddeed35f1d7b56abd54deaaf4cf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bffe378e1bcb2589dcd0ca95cc72593",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f1569c05631cdd5f3eae",
+    "url": "/monsters-rolodex/static/css/main.bb3584e8.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f1569c05631cdd5f3eae",
+    "url": "/monsters-rolodex/static/js/main.4ec73a94.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
